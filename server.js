3e4314f839b5e506bdb0fd64e362d3b783c8c4b5
cha <chyb627@gmail.com>
2022-01-13 12:59:31 +0900

    수정 : server.js 와 main.html 연결

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.listen(8080, function() {
     console.log('listening on 8080')
 })
 
-app.get('/pet', function(요청, 응답) { 
-    응답.send('펫용품 사시오')
+app.get('/', function(요청, 응답) { 
+    응답.sendFile(__dirname +'/main.html')
   })
--- study.md
+++ study.md
@@ -25,3 +25,8 @@
 4. npm install express --> node_modules 설치, package-lock.json 설치
 
 - express가 뭐냐 : 서버를 매우 쉽게 짤 수 있게 도와주는 라이브러리입니다. 덕분에 코드 두줄 쓰면 서버 생성 끝임
+
+
+#### 실행
+node server.js
+nodemon server.js 
